761fcc45e7044f78640bd34a41d0473f379367d7
PellegrinelliSean <s.pellegrinelli2@studenti.unibg.it>
2023-01-28 17:05:49 +0100

    Setup ambiente per test di unità

--- Workspace/WebServer/src/test/java/com/ourbooks/code/WebServerApplicationTests.java
+++ Workspace/WebServer/src/test/java/com/ourbooks/code/WebServerApplicationTests.java
@@ -1,13 +1,23 @@
 package com.ourbooks.code;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class WebServerApplicationTests {
 
+	@Value(value="${local.server.port}")
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
 	@Test
-	void contextLoads() {
+	public void temptest() throws Exception {
 	}
 
 }
