4ec0b378a3bffc73d7105aacebc8c1594b7e9590
PellegrinelliSean <s.pellegrinelli2@studenti.unibg.it>
2023-01-30 10:53:25 +0100

    Implementazione test per registrazione e login

--- Workspace/WebServer/src/test/java/com/ourbooks/code/WebServerApplicationTests.java
+++ Workspace/WebServer/src/test/java/com/ourbooks/code/WebServerApplicationTests.java
@@ -1,38 +1,100 @@
 package com.ourbooks.code;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
-import com.ourbooks.code.domain.utenti.CondLibro;
-import com.ourbooks.code.domain.utenti.ServizioLibri;
+import com.ourbooks.code.domain.utenti.DtoUtente;
 import com.ourbooks.code.domain.utenti.ServizioUtenti;
+import com.ourbooks.code.domain.utenti.Utente;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class WebServerApplicationTests {
-
-	@Value(value="${local.server.port}")
-	private int port;
-
+	
 	@Autowired
 	private TestRestTemplate restTemplate;
+	
+	//ServizioUtenti usato solo per eliminare dal database i dati inseriti nello
+	//stesso durante i test
 	@Autowired
-	ServizioUtenti servizioU;
-	@Autowired
-	ServizioLibri servizioL;
+	private ServizioUtenti servizioU; 
 	
 	@Test
-	public void temptest() {
-//		servizioU.creaAccount("nico.pellenp@gmail.com", "passwordprova", 35.5, 4.6, 10, "Lo Hobbit", "V per Vendetta");
-//		String id = servizioU.login("nico.pellenp@gmail.com", "passwordprova");
-//		servizioL.aggiungiLibro(id, "Tre Joker", 250, 2020, CondLibro.BUONE, true);
-//		servizioL.aggiungiLibro(id, "Chtulhu", 400, 2018, CondLibro.OTTIME, false);
-//		servizioU.creaAccount("sean.pellegrinelli@gmail.com", "passwordprova", 12.5, -34.6, 5, "V per Vendetta", "Kingdome Come", "The Withcer", "Java concurrency in practice");
-//		id = servizioU.login("sean.pellegrinelli@gmail.com", "passwordprova");
-//		servizioL.aggiungiLibro(id, "Il signore degli anelli", 1200, 2015, CondLibro.BUONE, false);
+	public void testRegistrazioneLogin() {
+		String url_signup = "http://localhost:8080/signup";
+		String url_login = "http://localhost:8080/login";
+		
+		//REGISTRA UTENTE 1
+		//creazione dto utente 1
+		DtoUtente u_dto = new DtoUtente();
+		u_dto.setEmail("utente1@gmail.com");
+		u_dto.setPassword("psw_u1");
+		u_dto.setLat(45.44);
+		u_dto.setLon(10.14);
+		u_dto.setMaxDist(10);
+		u_dto.setLibriDesiderati(new String[]{"It", "Se questo è un uomo"});
+		//creazione richiesta http
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<DtoUtente> httpEntity_signup = new HttpEntity<>(u_dto, headers);
+		//invio richiesta
+		ResponseEntity<String> response = restTemplate.exchange(url_signup, HttpMethod.POST, httpEntity_signup, String.class);
+		//verifica correttezza risposta
+		assertEquals("200 OK", response.getStatusCode().toString());
+		
+		//REGISTRAZIONE UTENTE CON STESSA EMAIL
+		response = restTemplate.exchange(url_signup, HttpMethod.POST, httpEntity_signup, String.class);
+		assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
+		
+		//LOGIN UTENTE 1
+		//creazione credenziali
+		Map<String, String> credenziali = new HashMap<String, String>();
+		credenziali.put("email", u_dto.getEmail());
+		credenziali.put("password", u_dto.getPassword());
+		//creazione richiesta http
+		headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Map<String, String>> httpEntity_login = new HttpEntity<>(credenziali, headers);
+		//invio richiesta
+		Utente u = restTemplate.postForObject(url_login, httpEntity_login, Utente.class);
+		//verifica che l'utente ritornato abbia le stesse info di u1_dto
+		assertEquals(u_dto.getEmail(), u.getEmail());
+		assertEquals(u_dto.getPassword(), u.getPassword());
+		assertEquals(u_dto.getLat(), u.getLat(), 0);
+		assertEquals(u_dto.getLon(), u.getLon(), 0);
+		assertEquals(u_dto.getMaxDist(), u.getMaxDist(), 0);
+		assertArrayEquals(new String[]{u_dto.getLibriDesiderati()[0], u_dto.getLibriDesiderati()[1], null}, u.getLibriDesiderati());
+		assertEquals(0, u.getnToken());
+		
+		//LOGIN UTENTE 1 CREDENZIALI ERRATE
+		credenziali = new HashMap<String, String>();
+		credenziali.put("email", "errata@gmail.com");
+		credenziali.put("password", u_dto.getPassword());
+		httpEntity_login = new HttpEntity<>(credenziali, headers);
+		response = restTemplate.exchange(url_login, HttpMethod.POST, httpEntity_login, String.class);
+		assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
+		
+		credenziali = new HashMap<String, String>();
+		credenziali.put("email", u_dto.getEmail());
+		credenziali.put("password", "psw_err");
+		httpEntity_login = new HttpEntity<>(credenziali, headers);
+		response = restTemplate.exchange(url_login, HttpMethod.POST, httpEntity_login, String.class);
+		assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
+		
+		//CANCELLO UTENTE 1
+		servizioU.eliminaAccount(u.getId());
 	}
 
 }
